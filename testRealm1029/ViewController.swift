977bc2b837a33913253306ffa70f60f6daba8708
yutaka takagaki <takagakiyutaka@takagaki-no-mba.local>
2018-10-30 09:33:51 +0900

    change object

--- testRealm1029/ViewController.swift
+++ testRealm1029/ViewController.swift
@@ -15,10 +15,23 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     let realm = try! Realm()
-        let person = Person(value: ["name" : "Yu", "age":32])
-    
+//        let person = Person(value: ["name" : "Yu", "age":32])
+//        let cats = [Cat(value: ["name": "Toto", "age": 1]),
+//                    Cat(value: ["name": "Rao", "age": 3])]
+        
+        let dictionary: [String: Any] = ["name": "Yu",
+                                         "age": 32,
+                                         "dog": ["name": "Momo", "age": 9],
+                                         "cats":[["name": "Toto", "age": 1],
+                                                 ["name" : "Rao", "age": 2]]]
+        let person = Person(value: dictionary)
+            
         try! realm.write {
+            realm.deleteAll()
+//            realm.add(person)
+//            realm.add(cats)
             realm.add(person)
+            
         }
         
         print(Realm.Configuration.defaultConfiguration.fileURL!)
